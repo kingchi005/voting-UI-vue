25838e8cde1c43a6b517774f891886ca98854a1d
Kingchi <kingchi005@gmail.com>
2023-02-14 21:24:13 +0100

    addedprops to AspirantForm

---
+++ src/components/AspirantForm.vue
@@ -0,0 +1,149 @@
+<template>
+  <v-sheet width="350" class="mx-auto pa-5">
+    <v-form ref="form" @submit.prevent="handleSubmit">
+      <div class=" grey--text text-h5 font-weight-bold">{{ type == 'update_form' ? 'Update Aspirant' : 'Add an Aspirant'}}</div>
+      <v-alert class="mb-3 py-1" v-if="alert_msgs.length != 0" :type="alert_type" title="" variant="tonal" border="start" close-label="Close Alert">
+        <div v-for="(item, i) in alert_msgs" :key="i">{{item}}</div>
+      </v-alert>
+      <v-text-field v-model="first_name" :rules="first_name_rules" label="First name"></v-text-field>
+      <v-text-field v-model="other_names" :rules="other_names_rules" label="Other names"></v-text-field>
+      <v-text-field v-model="department" :rules="department_rules" label="Department"></v-text-field>
+      <v-select v-model="office_id" :items="offices" item-title="name" item-value="id" label="Select office" persistent-hint return-object single-line></v-select>
+      <v-file-input :rules="avatar_rules" accept="image/png, image/jpeg, image/jpg" placeholder="Pick an avatar" label="Avatar" @change="uploadImage" show-size></v-file-input>
+      <v-btn type="submit" block rounded="lg" color="primary">{{ type == 'update_form' ? 'Update Aspirant' : 'Create Aspirant'}}</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+<script>
+import { onMounted } from 'vue'
+export default {
+  props: ["type","init_first_name" ,"init_other_names","init_department","init_avatar","init_office_id"]
+  , data: () => ({
+    alert_msgs: []
+      // alert_msgs: ['"department" is not allowed to be empty sokjhdf dont', ' "avatar" ijdhfs not allowed to be empty', "odhjfnhduhj"]
+    , alert_type: ''
+    , first_name: ''
+    , other_names: ''
+    , department: ''
+    , avatar: ''
+    , office_id: { name: "Select an office", id: 9 }
+    , offices: [ /*      { "id": 1, "_id": "039174dd9b734d714", "name": "SEC GEN", "deleted_flag": false, "createdAt": "2023-02-06T20:09:58.000Z", "updatedAt": "2023-02-06T20:09:58.000Z" } , { "id": 2, "_id": "07a6ba908e348cd81", "name": "President", "deleted_flag": false, "createdAt": "2023-02-06T20:10:13.000Z", "updatedAt": "2023-02-06T20:10:13.000Z" } , { "id": 3, "_id": "065b491048029ea98", "name": "pro", "deleted_flag": false, "createdAt": "2023-02-06T20:10:24.000Z", "updatedAt": "2023-02-06T20:10:24.000Z" } , { "id": 4, "_id": "04fd4623ad663594b", "name": "DOI", "deleted_flag": false, "createdAt": "2023-02-06T20:10:34.000Z", "updatedAt": "2023-02-06T20:10:34.000Z" } , { "id": 6, "_id": "06230f29a9a92f7b4", "name": "test office", "deleted_flag": false, "createdAt": "2023-02-07T11:14:34.000Z", "updatedAt": "2023-02-07T11:14:34.000Z" } */ ]
+    , first_name_rules: [
+      value => value ? true : 'First name is required'
+    ]
+    , other_names_rules: [
+      value => value ? true : 'Enter at least other name.'
+    ]
+    , department_rules: [
+      value => value ? true : 'Department is required.'
+    ]
+    , select_rules: [
+      value => value ? true : 'You must enter a first name.'
+    ]
+    , avatar_rules: [
+      value => !value || !value.length || value[0].size < 150000 || 'Avatar size should be less than 150KB!'
+    ]
+  })
+  , methods: {
+    async handleSubmit(e) {
+      this.alert_msgs = []
+      // console.log(this.$refs.form)
+      // if (!this.$refs.form.validate()) return false
+      if (this.$refs.form.validate()) {
+        const req = {
+          first_name: this.first_name
+          , other_names: this.other_names
+          , department: this.department
+          , office_id: this.office_id.id
+          , avatar: this.avatar
+        }
+        const _this_ = this
+        if (this.type == "update_form") {
+          update_aspirant(_this_)
+        }else{
+          create_aspirant(_this_)
+        }
+        async function create_aspirant(_this) {
+          const res = await fetch("http://127.0.0.1:500/admin/create-aspirant", {
+            method: "POST"
+            , body: JSON.stringify(req)
+            , headers: {
+              "content-Type": "application/json"
+            }
+          })
+          const result = await res.json()
+          if (result.ok) {
+            _this.alert_msgs.append(result.msg)
+            _this.alert_type = "success"
+            // console.log(result)
+          } else {
+            _this.alert_type = "error"
+            result.msg = result.msg.split(".")
+            // console.log(result.msg)
+            _this.alert_msgs = result.msg
+            // console.log(result.msg)
+          }
+        }
+        function update_aspirant () {
+
+          console.log(req)
+        }
+      }
+    }
+    // ,update_aspirant(){
+    //   this.alert_msgs = []
+    //   // console.log(this.$refs.form)
+    //   // if (!this.$refs.form.validate()) return false
+    //   if (this.$refs.form.validate()) {
+    //     const req = {
+    //       first_name: this.first_name
+    //       , other_names: this.other_names
+    //       , department: this.department
+    //       , office_id: this.office_id.id
+    //       , avatar: this.avatar
+    //     }
+    //   }
+    // }
+    , convertBase64(file) {
+      return new Promise((resolve, reject) => {
+        const fileReader = new FileReader()
+        fileReader.readAsDataURL(file)
+
+        fileReader.onload = () => {
+          resolve(fileReader.result)
+        }
+
+        fileReader.onerror = err => {
+          reject(err)
+        }
+      })
+    }
+    , async uploadImage(e) {
+      // console.log('changgeed')
+      const file = e.target.files[0]
+      const base64 = await this.convertBase64(file)
+      this.avatar = base64
+    }
+  }
+  , async mounted() {
+    // any JavaScript Code
+    const res = await fetch("http://127.0.0.1:500/admin/fetch-office/")
+    const result = await res.json()
+    if (result.ok) {
+      this.offices = result.offices
+    }
+    this.first_name = this.type == "update_form" ? this.init_first_name : ''
+    this.other_names = this.type == "update_form" ? this.init_other_names : ''
+    this.department = this.type == "update_form" ? this.init_department : ''
+    this.office_id = this.type == "update_form" ? this.init_office_id : this.office_id
+    // this.avatar = this.type == "update_form" ? this.init_avatar : ''
+    // console.log(this.alert_msgs)
+  }
+  , computed() {
+    console.log(this.type)
+    // this.first_name = this.initial_form_data || ''
+    // this.alert_msgs = this.alert_msgs != "" ? "this is it" : ""
+  }
+
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
-<script setup>
-  import TheWelcome from '../components/TheWelcome.vue'
-  import Form from '../components/form.vue'
-</script>
-
 <template>
   <main>
-    <Form />
-    <!-- <TheWelcome /> -->
+    <AspirantForm />
+    <AspirantForm type="update_form"
+      :init_first_name="initial_form_data.init_first_name"
+      :init_other_names="initial_form_data.init_other_names"
+      :init_department="initial_form_data.init_department"
+      :init_office_id="initial_form_data.init_office_id"
+      :init_avatar="initial_form_data.init_avatar"
+     />
   </main>
 </template>
+<script>
+import AspirantForm from '../components/AspirantForm.vue'
+
+export default {
+  components: { AspirantForm }
+  , data: () => ({
+    initial_form_data: {
+      init_first_name: "Amanda"
+      , init_other_names: "Love"
+      , init_department: "Beauty department"
+      , init_office_id: { name: "i Love you girl", id: 30 }
+      , init_avatar: "test avatar "
+    }
+  })
+}
+</script>
